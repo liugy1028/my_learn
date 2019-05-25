00a55645832c99ff4564c87ff1aec2193fcaf14a
liuguanyin <841788285@qq.com>
2019-05-25 16:12:55 +0800

    修改appid值

--- application/common.php
+++ application/common.php
@@ -30,6 +30,25 @@
      return $data;
  }
 
+//curl get方式(https)
+function curl_https($url){
+    $curl = curl_init();
+    //设置抓取的url
+    curl_setopt($curl, CURLOPT_URL, $url);
+    //设置头文件的信息作为数据流输出
+    curl_setopt($curl, CURLOPT_HEADER, 0);
+    //设置获取的信息以文件流的形式返回，而不是直接输出。
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    //执行命令
+    $data = curl_exec($curl);
+    //关闭URL请求
+    curl_close($curl);
+    //显示获得的数据
+    $data =json_decode($data);
+    return $data;
+}
+
 
 //curl post 方式
 function curl_post($data,$url){
