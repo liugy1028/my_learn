11f5ecc1f8f0e6214aa539f1798975c7762aa633
liuguanyin <841788285@qq.com>
2019-05-25 16:08:27 +0800

    修改appid值

--- application/weixin/controller/Userinfo.php
+++ application/weixin/controller/Userinfo.php
@@ -13,7 +13,7 @@ class Userinfo extends  Controller
         $this->secret= config('secret');
     }
 
-
+    //获取用户信息
     public function userinfo()
     {
         $userinfo  =Session::get('userinfo');
@@ -30,10 +30,11 @@ class Userinfo extends  Controller
         }
     }
 
-
+    //获取微信code
     public function get_code(){
         $redirect  =$_SERVER['REQUEST_URI'].'&response_type=code&scope=SCOPE&state=STATE#wechat_redirect';
         $url       ='https://open.weixin.qq.com/connect/oauth2/authorize?appid='.$this->appid.'&redirect_uri='.$redirect;
+        var_dump($url);
         $code      =curl_get($url);
         var_dump($code);die;
     }
