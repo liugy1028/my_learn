0ad8de5e4b38b497ab78518fa2d6c91b57e7828a
liuguanyin <841788285@qq.com>
2019-05-25 16:59:39 +0800

    添加 curl支持https

--- application/weixin/controller/Userinfo.php
+++ application/weixin/controller/Userinfo.php
@@ -32,7 +32,11 @@ class Userinfo extends  Controller
 
     //获取微信code
     public function get_code(){
-        $redirect  =$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'].'&response_type=code&scope=SCOPE&state=STATE#wechat_redirect';
+        var_dump($_SERVER['https']);
+        if($_SERVER['https']=='off'){
+
+        }
+        $redirect  ='http'.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'].'&response_type=code&scope=SCOPE&state=STATE#wechat_redirect';
         $url       ='https://open.weixin.qq.com/connect/oauth2/authorize?appid='.$this->appid.'&redirect_uri='.$redirect;
         var_dump($url);
         $code      =http_content($url);
