e15e045104cc751646c498df17b732df7d0bb94d
liuguanyin <841788285@qq.com>
2019-05-30 11:20:12 +0800

    获取access_token

--- application/weixin/controller/Weixinaction.php
+++ application/weixin/controller/Weixinaction.php
@@ -11,21 +11,21 @@ class Weixinaction
 {
     private  $appid;
     private  $secret;
-
+    private  $redis ;
     public function _initialize(){
         $this->appid  =config('appid');
         $this->secret =config('secret');
+        $this->redis  =new \Redis();
     }
 
 
     public function get_AccessToken(){
-        $redis  =new Redis();
-        $access =$redis->get('access_token_'.$this->appid);
+        $access =$this->redis->get('access_token_'.$this->appid);
         if(!$access){
             $url ='https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.$this->appid.'&secret='.$this->secret;
             $data=curl_https($url);
             if($data['access_token']){
-                $redis->setex('access_token_'.$this->appid,'7200',$data['access_token']);
+                $this->redis->setex('access_token_'.$this->appid,'7200',$data['access_token']);
                 return ['access_token'=>$data['access_token']];
             }else{
                 throw new Exception('获取access_token失败'.$data);
